b41741aa42aa2e198704a74b1a3df3499775b4b8
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-04-12 23:01:43 -0300

    Lógica dos anexos

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -1,5 +1,6 @@
 import {Format} from './../util/Format'
-import {CameraController} from './../controller/cameraController'
+import {CameraController} from './cameraController'
+import {DocumentPreviewController} from './DocumentPreviewController'
 
 
 export class WhatsAppController {
@@ -144,14 +145,67 @@ export class WhatsAppController {
             this._camera.stop()
         })
         this.el.btnTakePicture.on('click',e=>{
-            console.log('take picture')
+
+            console.log('picture')
+
+            let dataUrl = this._camera.takePicture() 
+            this.el.pictureCamera.src(dataUrl)
+
+            this.el.pictureCamera.show()
+            this.el.videoCamera.hide()
+            this.el.btnReshootPanelCamera.show()
+            this.el.containerTakePicture.hide()
+            this.el.containerSendPicture.show()
         })
+
+        this.el.btnReshootPanelCamera.on('click',e=>{
+            this.el.pictureCamera.hide()
+            this.el.videoCamera.show()
+            this.el.btnReshootPanelCamera.hide()
+            this.el.containerTakePicture.show()
+            this.el.containerSendPicture.hide()
+        })
+
+        this.el.btnSendPicture.on('click',e=>{
+            console.log(this.el.pictureCamera.src)
+        })
+
         this.el.btnAttachDocument.on('click',e=>{
             this.closeAllMainPanel()
             this.el.panelDocumentPreview.addClass('open')
             this.el.panelDocumentPreview.css({
                 'height': '100%'
             });
+            this.el.inputDocument.click()
+        })
+
+        this.el.inputDocument.on('change',e=>{
+            if(this.el.inputDocument.files.length){
+                let file = this.el.inputDocument.files[0]
+                this._documentPreviewController = new DocumentPreviewController(file)
+                this._documentPreviewController.getPreviewData().then(result=>{
+                    this.el.imgPanelDocumentPreview.src = result.src
+                    this.el.infoPanelDocumentPreview.innerHTML = result.info
+                    this.el.imagePanelDocumentPreview.show()
+                    this.el.filePanelDocumentPreview.hide()
+
+                }).catch(err=>{
+                    console.log(file.type)
+                    switch(file.type){
+                        case 'application/vnd.ms-excel':
+                            this.el.iconPanelDocumentPreview.className= 'jcxhw icon-doc-xls'
+
+
+                        break;
+
+                        default:
+                            this.el.iconPanelDocumentPreview.className= 'jcxhw icon-doc-generic'
+                    }
+                    this.el.filenamePanelDocumentPreview.innerHTML = file.name
+                    this.el.imagePanelDocumentPreview.hide()
+                    this.el.filePanelDocumentPreview.show()
+                })
+            }
         })
         this.el.btnClosePanelDocumentPreview.on('click',e=>{
             this.closeAllMainPanel()
