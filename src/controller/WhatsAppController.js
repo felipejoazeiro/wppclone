5f13c0f88ea31f19d836ba5fa7a61991b4d5d9c7
Felipe Joazeiro <felipejoazeiro@gmail.com>
2021-04-15 13:22:03 -0300

    email on FireBase done!

--- src/controller/WhatsAppController.js
+++ src/controller/WhatsAppController.js
@@ -3,6 +3,7 @@ import {CameraController} from './cameraController'
 import {MicrophoneController} from './MicrophoneController'
 import {DocumentPreviewController} from './DocumentPreviewController'
 import {Firebase} from '../util/Firebase'
+import { User } from '../model/User'
 
 export class WhatsAppController {
     constructor(){
@@ -17,10 +18,22 @@ export class WhatsAppController {
 
     initAuth(){
         this._firebase.initAuth().then(response=>{
-            this._user = response._user
-            this.el.appContent.css({
-                display: 'flex'
+            this._user = new User()
+
+            let userRef = User.findByEmail(response.user.email)
+
+            userRef.set({
+                name: response.user.displayName,
+                email: response.user.email,
+                photo: response.user.photoURL
+            }).then(()=>{
+                this.el.appContent.css({
+                    display: 'flex'
+                })
+
             })
+
+            
         }).catch(err=>{
             console.error(err)
         })
--- src/model/User.js
+++ src/model/User.js
@@ -5,4 +5,7 @@ export class User extends ClassEvent{
     static getRef(){
         return Firebase.db().collection('/users')
     }
+    static findByEmail(email){
+        return User.getRef().doc(email)
+    }
 }
